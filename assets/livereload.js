ff7b24bcca17202062d42d15b8730aecfa9cf918
DiogoAMFerreira <DiogoAMFerreira@users.noreply.github.com>
2025-11-04 15:07:05 +0000

    Setting up the server

---
+++ assets/livereload.js
@@ -0,0 +1,9 @@
+// This script injects a live reload WebSocket client into HTML pages
+(function() {
+  var ws = new WebSocket('ws://' + location.host);
+  ws.onmessage = function(event) {
+    if (event.data === 'reload') {
+      location.reload();
+    }
+  };
+})();
