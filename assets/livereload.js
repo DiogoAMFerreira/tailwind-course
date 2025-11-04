1b46dfba6793813611cbf187ba903f9c6ea5590b
DiogoAMFerreira <DiogoAMFerreira@users.noreply.github.com>
2025-11-04 15:07:28 +0000

    Changes for Live Server

--- assets/livereload.js
+++ assets/livereload.js
@@ -1,8 +1,8 @@
 // This script injects a live reload WebSocket client into HTML pages
-(function() {
-  var ws = new WebSocket('ws://' + location.host);
-  ws.onmessage = function(event) {
-    if (event.data === 'reload') {
+(function () {
+  var ws = new WebSocket("ws://" + location.host);
+  ws.onmessage = function (event) {
+    if (event.data === "reload") {
       location.reload();
     }
   };
--- dev-server.js
+++ dev-server.js
@@ -1,9 +1,9 @@
 // Simple dev server with live reload for static files
-const express = require('express');
-const path = require('path');
-const http = require('http');
-const WebSocket = require('ws');
-const chokidar = require('chokidar');
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const WebSocket = require("ws");
+const chokidar = require("chokidar");
 
 const app = express();
 const server = http.createServer(app);
@@ -12,11 +12,11 @@ const PORT = process.env.PORT || 3000;
 
 // Serve static files
 app.use(express.static(__dirname));
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use("/assets", express.static(path.join(__dirname, "assets")));
 
 // Inject live reload script into HTML responses
 app.use((req, res, next) => {
-  if (req.path.endsWith('.html')) {
+  if (req.path.endsWith(".html")) {
     const filePath = path.join(__dirname, req.path);
     res.sendFile(filePath, {}, (err) => {
       if (!err) {
@@ -30,19 +30,26 @@ app.use((req, res, next) => {
   }
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '03-container-spacing', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "03-container-spacing", "index.html"));
 });
 
 // Watch for file changes and notify clients
-chokidar.watch([path.join(__dirname, '**/*.html'), path.join(__dirname, 'assets/**/*')]).on('change', () => {
-  wss.clients.forEach(client => {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send('reload');
-    }
+chokidar
+  .watch([
+    path.join(__dirname, "**/*.html"),
+    path.join(__dirname, "assets/**/*"),
+  ])
+  .on("change", () => {
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send("reload");
+      }
+    });
   });
-});
 
 server.listen(PORT, () => {
-  console.log(`Dev server with live reload running at http://localhost:${PORT}`);
+  console.log(
+    `Dev server with live reload running at http://localhost:${PORT}`
+  );
 });
