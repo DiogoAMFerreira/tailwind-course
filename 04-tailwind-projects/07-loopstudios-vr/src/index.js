45a5df8009c90bea90e15f21b4f2fcff7968ad39
DiogoAMFerreira <DiogoAMFerreira@users.noreply.github.com>
2025-11-27 16:50:18 +0000

    49 - Hamburger Button - Done with SVG instead of Spans

--- 04-tailwind-projects/07-loopstudios-vr/src/index.js
+++ 04-tailwind-projects/07-loopstudios-vr/src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+
+// Hamburger to X toggle
+const hamIcons = document.getElementsByClassName('hamburger-icon');
+for (let i = 0; i < hamIcons.length; i++) {
+    hamIcons[i].addEventListener('click', () => {
+        hamIcons[i].classList.toggle('active');
+    });
+}
