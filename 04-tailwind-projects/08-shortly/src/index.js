b1a2df962e913d1fe2be328338049aa898d5ab2d
DiogoAMFerreira <DiogoAMFerreira@users.noreply.github.com>
2025-11-28 13:55:23 +0000

    60 - Mobile menu

--- 04-tailwind-projects/08-shortly/src/index.js
+++ 04-tailwind-projects/08-shortly/src/index.js
@@ -35,3 +35,19 @@ function validUrkl(str) {
     );
     return !!pattern.test(str);
 }
+
+// Hamburger to X toggle
+const hamIcons = document.getElementsByClassName('hamburger-icon');
+for (let i = 0; i < hamIcons.length; i++) {
+    hamIcons[i].addEventListener('click', () => {
+        hamIcons[i].classList.toggle('active');
+    });
+}
+
+const menuBtn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('flex');
+    menu.classList.toggle('hidden');
+});
