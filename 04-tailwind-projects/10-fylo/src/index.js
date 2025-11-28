949e2ffe1ab92118ca75c96ec36e532c78e93424
DiogoAMFerreira <DiogoAMFerreira@users.noreply.github.com>
2025-11-28 17:33:40 +0000

    70 - Ligh and Dark Mode

--- 04-tailwind-projects/10-fylo/src/index.js
+++ 04-tailwind-projects/10-fylo/src/index.js
@@ -1,1 +1,26 @@
 import './style.css';
+
+const themeToggleBtn = document.getElementById('theme-toggle');
+const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+if (
+    localStorage.getItem('color-theme') === 'dark' ||
+    (!('color-theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
+) {
+    // Show light icon
+    themeToggleLightIcon.classList.remove('hidden');
+} else {
+    // Show dark icon
+    themeToggleDarkIcon.classList.remove('hidden');
+}
+
+themeToggleBtn.addEventListener('click', () => {
+    // Toggle icons inside button
+    themeToggleDarkIcon.classList.toggle('hidden');
+    themeToggleLightIcon.classList.toggle('hidden');
+
+    const isDark = document.documentElement.classList.toggle('dark');
+    localStorage.setItem('color-theme', isDark ? 'dark' : 'light');
+});
