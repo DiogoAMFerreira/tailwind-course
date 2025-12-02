356333f92105b27f0838e68c3e23ea1258fb8e06
DiogoAMFerreira <DiogoAMFerreira@users.noreply.github.com>
2025-12-02 11:40:40 +0000

    76 - Setup & Config

--- 04-tailwind-projects/11-bookmark/tailwind.config.js
+++ 04-tailwind-projects/11-bookmark/tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
     content: ['./dist/*.{html,js}', './src/*.{html,js}'],
     theme: {
-        extend: {},
+        screens: {
+            sm: '480px',
+            md: '768px',
+            lg: '1020px',
+            xl: '1440px',
+        },
+        extend: {
+            colors: {
+                'soft-blue': 'hsl(231, 69%, 60%)',
+                'soft-red': 'hsl(0, 94%, 66%)',
+                'grayish-blue': 'hsl(229, 8%, 60%)',
+                'very-dark-blue': 'hsl(229, 31%, 21%)',
+            },
+            fontFamily: {
+                sans: ['Rubik', 'sans-serif'],
+            },
+        },
     },
     plugins: [],
 };
